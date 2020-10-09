7c8ac9dd722f8e95b5c16bf57660811bdd7822f3
Jonas Haist <72508578+JonasHaist@users.noreply.github.com>
2020-10-09 11:08:45 +0300

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -1,22 +1,25 @@
 #ifdef _WIN32
-#include <Windows.h>
+#include <Windows.h> //This allows the program to run on Windows
 #else
-#include <unistd.h>
+#include <unistd.h> //This allows the program to run on Linux/Unix systems 
 #endif
 #include <iostream>
-#include <cstdlib>
+#include <cstdlib> //The cstdlib library adds the 'sleep' function
+
 using namespace std;
-int main() {
+int main()
+{
 int w;
 cout << "Enter your weight ";
-cin >> w;
+cin >> w; //The user inputs the variable's value
 cout << "Calculating";
-for(int a=0; a<7; a++){
+for(int a=0; a<7; a++) //Our program enters a loop
+{
 cout.flush();
-sleep(1);
+sleep(1); //The program is put on hold for a given amout of time, in this instance one second
 cout << ".";
 }
-cout<< "ur fat" << endl;
+cout<< "ur fat" << endl; //The program exits the loop and prints the final text, after which the program returns 0 and terminates
 return 0;
 }
 //Enjoy
