37c3d3c39506c5109594590bc5438892fca85e69
Jonas Haist <72508578+JonasHaist@users.noreply.github.com>
2021-08-11 10:25:14 -0400

    Update main.cpp
    Dropped support for windows. Added support for pounds and kilograms.

--- main.cpp
+++ main.cpp
@@ -1,17 +1,33 @@
-#ifdef _WIN32
-#include <Windows.h> //This allows the program to run on Windows
-#else
 #include <unistd.h> //This allows the program to run on Linux/Unix systems 
-#endif
 #include <iostream>
 #include <cstdlib> //The cstdlib library adds the 'sleep' function
 
 using namespace std;
 int main()
 {
-int w;
-cout << "Enter your weight ";
-cin >> w; //The user inputs the variable's value
+int w, wl; // w will be the weight in kilograms, wl will be the weight in pounds
+  string select;
+  
+cout << "Would you like to use pounds, or kilograms? Input 'lbs' or 'kg'" << endl;
+  cin >> select;
+    cout << "Enter your weight ";
+      cin >> w; //The user inputs the variable's value
+  
+  
+if( select == "lbs")
+      w = w/2; // We convert pounds to kilos
+  
+else if ( select == "kg")
+      w = w; // If the user chose kilos, we change nothing.
+  
+else {
+while ( 1>0) { //Our program enters a loop, that lasts forever
+{
+    cout.flush();
+      sleep(1); //The program is put on hold for a given amout of time, in this instance one second
+    cout << "ERROR 0001. WEIGHT UNIT DOES NOT EXIST";
+    }
+      } 
 cout << "Calculating";
 for(int a=0; a<7; a++) //Our program enters a loop
 {
